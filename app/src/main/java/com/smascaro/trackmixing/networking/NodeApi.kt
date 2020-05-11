620026fc44c6e58923c3e38a2db2645ce52ac88c
Sergi <msk1416@gmail.com>
2020-05-12 00:02:53 +0200

    Add api method on Retrofit Api interface

--- app/src/main/java/com/smascaro/trackmixing/networking/NodeApi.kt
+++ app/src/main/java/com/smascaro/trackmixing/networking/NodeApi.kt
@@ -1,8 +1,10 @@
 package com.smascaro.trackmixing.networking
 
 import com.smascaro.trackmixing.networking.availableTracks.AvailableTracksResponseSchema
+import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.Call
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NodeApi {
@@ -12,4 +14,7 @@ interface NodeApi {
         @Query("limit") limit: Int?,
         @Query("offset") offset: Int?
     ): Call<AvailableTracksResponseSchema>
+
+    @GET("/fetch/{videoId}")
+    fun fetchSong(@Path("videoId") videoId: String): Call<ResponseBody>
 }
