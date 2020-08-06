adbcaedcb37c88e280ea762db38a128345da48c4
Sergi <msk1416@gmail.com>
2020-08-06 22:05:43 +0200

    Inject MainActivityController and its dependencies by dagger

--- app/src/main/java/com/smascaro/trackmixing/common/TrackMixingApplication.kt
+++ app/src/main/java/com/smascaro/trackmixing/common/TrackMixingApplication.kt
@@ -1,7 +1,9 @@
 package com.smascaro.trackmixing.common
 
 import android.app.Application
+import com.smascaro.trackmixing.common.di.AppComponent
 import com.smascaro.trackmixing.common.di.CompositionRoot
+import com.smascaro.trackmixing.common.di.DaggerAppComponent
 import timber.log.Timber
 
 class TrackMixingApplication : Application() {
@@ -13,6 +15,11 @@ class TrackMixingApplication : Application() {
 
         Timber.plant(Timber.DebugTree())
     }
+
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(applicationContext)
+    }
+
 
     fun getCompositionRoot(): CompositionRoot {
         return mCompositionRoot
--- app/src/main/java/com/smascaro/trackmixing/ui/main/MainActivity.kt
+++ app/src/main/java/com/smascaro/trackmixing/ui/main/MainActivity.kt
@@ -7,14 +7,16 @@ import com.smascaro.trackmixing.R
 import com.smascaro.trackmixing.common.TrackMixingApplication
 import com.smascaro.trackmixing.ui.common.BaseActivity
 import timber.log.Timber
+import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
-    private lateinit var mController: MainActivityController
+    @Inject
+    lateinit var mController: MainActivityController
     override fun onCreate(savedInstanceState: Bundle?) {
+        (application as TrackMixingApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
         val viewMvc = getCompositionRoot().getViewMvcFactory().getMainActivityViewMvc(null)
-        mController = getCompositionRoot().getMainActivityController()
 
         mController.bindViewMvc(viewMvc)
         mController.handleIntent(intent)
