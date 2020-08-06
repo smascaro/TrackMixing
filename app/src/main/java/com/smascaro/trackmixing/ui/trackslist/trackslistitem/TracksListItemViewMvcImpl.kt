1ae81854079cb3c2616cbb37a39f01ce03d07a05
Sergi <msk1416@gmail.com>
2020-08-06 22:06:49 +0200

    Rename BaseApplication to `TrackMixingApplication` (cont.)

--- app/src/main/java/com/smascaro/trackmixing/ui/trackslist/trackslistitem/TracksListItemViewMvcImpl.kt
+++ app/src/main/java/com/smascaro/trackmixing/ui/trackslist/trackslistitem/TracksListItemViewMvcImpl.kt
@@ -7,12 +7,15 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.textview.MaterialTextView
 import com.smascaro.trackmixing.R
+import com.smascaro.trackmixing.common.di.DaggerAppComponent
 import com.smascaro.trackmixing.tracks.Track
 import com.smascaro.trackmixing.ui.common.BaseObservableViewMvc
+import javax.inject.Inject
 
 class TracksListItemViewMvcImpl(inflater: LayoutInflater, parent: ViewGroup?) :
     BaseObservableViewMvc<TracksListItemViewMvc.Listener>(), TracksListItemViewMvc {
@@ -26,11 +29,14 @@ class TracksListItemViewMvcImpl(inflater: LayoutInflater, parent: ViewGroup?) :
 
     private var mPosition: Int = -1
 
+    lateinit var glideRequestManager: RequestManager
+
     //    private lateinit var mParentLayout: LinearLayout
     private var mIsExpanded: Boolean = false
 
     init {
         setRootView(inflater.inflate(R.layout.item_track, parent, false))
+        glideRequestManager = Glide.with(getContext()!!)
         mCard = findViewById(R.id.cardViewContainer)
         mTrackTitleTxt = findViewById(R.id.trackTitle)
         mTrackThumbnailImg = findViewById(R.id.thumbnailImg)
@@ -62,8 +68,7 @@ class TracksListItemViewMvcImpl(inflater: LayoutInflater, parent: ViewGroup?) :
         } else {
             View.GONE
         }
-        Glide
-            .with(getRootView().context)
+        glideRequestManager
             .asBitmap()
             .load(mTrack.thumbnailUrl)
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
