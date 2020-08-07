867b5c8bb619066a062de6f879d0e3721d4caa03
Sergi <msk1416@gmail.com>
2020-08-07 18:14:33 +0200

    Finish refactoring to dagger MainActivity

--- app/src/main/java/com/smascaro/trackmixing/ui/main/MainActivity.kt
+++ app/src/main/java/com/smascaro/trackmixing/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.smascaro.trackmixing.ui.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.smascaro.trackmixing.R
 import com.smascaro.trackmixing.common.TrackMixingApplication
 import com.smascaro.trackmixing.ui.common.BaseActivity
@@ -17,9 +18,11 @@ class MainActivity : BaseActivity() {
         (application as TrackMixingApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
+        val rootView = LayoutInflater.from(this).inflate(R.layout.activity_main, null, false)
+        viewMvc.bindRootView(rootView)
         mainActivityController.bindViewMvc(viewMvc)
         mainActivityController.handleIntent(intent)
 
-        setContentView(R.layout.activity_main)
+        setContentView(rootView)
     }
 }
