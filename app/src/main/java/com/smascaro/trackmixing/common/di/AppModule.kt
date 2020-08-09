f8d4deb5c6844251de5e021eec46c64896d8f535
Sergi <msk1416@gmail.com>
2020-08-09 10:44:46 +0200

    Provide Glide instance to notification helper via dagger

--- app/src/main/java/com/smascaro/trackmixing/common/di/AppModule.kt
+++ app/src/main/java/com/smascaro/trackmixing/common/di/AppModule.kt
@@ -1,13 +1,17 @@
 package com.smascaro.trackmixing.common.di
 
-import com.smascaro.trackmixing.common.NODE_BASE_URL
-import com.smascaro.trackmixing.common.di.main.RetrofitForJsonData
-import com.smascaro.trackmixing.networking.NodeApi
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import dagger.Module
 import dagger.Provides
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-class AppModule
+class AppModule {
+    @Singleton
+    @Provides
+    fun provideGlideInstance(context: Context): RequestManager {
+        return Glide.with(context)
+    }
+}
