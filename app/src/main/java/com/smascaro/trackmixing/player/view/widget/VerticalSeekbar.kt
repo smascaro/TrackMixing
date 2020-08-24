314a20e8d64fb6112d42bacab6dce0569540095d
Sergi <msk1416@gmail.com>
2020-08-24 12:39:08 +0200

    Add custom seekbar

---
+++ app/src/main/java/com/smascaro/trackmixing/player/view/widget/VerticalSeekbar.kt
@@ -0,0 +1,48 @@
+package com.smascaro.trackmixing.player.view.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatSeekBar
+
+class VerticalSeekbar : AppCompatSeekBar {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet, defstyle: Int) : super(
+        context,
+        attributeSet,
+        defstyle
+    )
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(h, w, oldh, oldw)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(measuredHeight, measuredWidth)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.rotate(-90f)
+        canvas?.translate(-height.toFloat(), 0f)
+        super.onDraw(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (!isEnabled) {
+            false
+        } else {
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN,
+                MotionEvent.ACTION_MOVE,
+                MotionEvent.ACTION_UP -> {
+                    progress = max - (max * event.y / height).toInt()
+                    onSizeChanged(width, height, 0, 0)
+                }
+            }
+            true
+        }
+    }
+}
