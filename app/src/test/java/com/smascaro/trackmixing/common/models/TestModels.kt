7463b1f039d3b901b083e670a15726c341000136
Sergi <msk1416@gmail.com>
2020-09-09 12:46:42 +0200

    Add more models and a list of entities

--- app/src/test/java/com/smascaro/trackmixing/common/models/TestModels.kt
+++ app/src/test/java/com/smascaro/trackmixing/common/models/TestModels.kt
@@ -19,16 +19,69 @@ class TestModels {
 
         fun getDownloadEntity(): DownloadEntity {
             return DownloadEntity(
-                DOWNLOAD_ENTITY_ID,
-                DOWNLOAD_ENTITY_VIDEO_KEY,
-                DOWNLOAD_ENTITY_QUALITY,
-                DOWNLOAD_ENTITY_TITLE,
-                DOWNLOAD_ENTITY_AUTHOR,
-                DOWNLOAD_ENTITY_THUMBNAIL_URL,
-                DOWNLOAD_ENTITY_REQUESTED_TIMESTAMP,
-                DOWNLOAD_ENTITY_DOWNLOAD_PATH,
-                DOWNLOAD_ENTITY_STATUS,
-                TRACK_SECONDS_LONG
+                DOWNLOAD_ENTITY_1_ID,
+                DOWNLOAD_ENTITY_1_VIDEO_KEY,
+                DOWNLOAD_ENTITY_1_QUALITY,
+                DOWNLOAD_ENTITY_1_TITLE,
+                DOWNLOAD_ENTITY_1_AUTHOR,
+                DOWNLOAD_ENTITY_1_THUMBNAIL_URL,
+                DOWNLOAD_ENTITY_1_REQUESTED_TIMESTAMP,
+                DOWNLOAD_ENTITY_1_DOWNLOAD_PATH,
+                DOWNLOAD_ENTITY_1_STATUS,
+                DOWNLOAD_ENTITY_1_SECONDS_LONG
+            )
+        }
+
+        fun getDownloadEntityList(): List<DownloadEntity> {
+            return listOf(
+                DownloadEntity(
+                    DOWNLOAD_ENTITY_1_ID,
+                    DOWNLOAD_ENTITY_1_VIDEO_KEY,
+                    DOWNLOAD_ENTITY_1_QUALITY,
+                    DOWNLOAD_ENTITY_1_TITLE,
+                    DOWNLOAD_ENTITY_1_AUTHOR,
+                    DOWNLOAD_ENTITY_1_THUMBNAIL_URL,
+                    DOWNLOAD_ENTITY_1_REQUESTED_TIMESTAMP,
+                    DOWNLOAD_ENTITY_1_DOWNLOAD_PATH,
+                    DOWNLOAD_ENTITY_1_STATUS,
+                    DOWNLOAD_ENTITY_1_SECONDS_LONG
+                ),
+                DownloadEntity(
+                    DOWNLOAD_ENTITY_2_ID,
+                    DOWNLOAD_ENTITY_2_VIDEO_KEY,
+                    DOWNLOAD_ENTITY_2_QUALITY,
+                    DOWNLOAD_ENTITY_2_TITLE,
+                    DOWNLOAD_ENTITY_2_AUTHOR,
+                    DOWNLOAD_ENTITY_2_THUMBNAIL_URL,
+                    DOWNLOAD_ENTITY_2_REQUESTED_TIMESTAMP,
+                    DOWNLOAD_ENTITY_2_DOWNLOAD_PATH,
+                    DOWNLOAD_ENTITY_2_STATUS,
+                    DOWNLOAD_ENTITY_2_SECONDS_LONG
+                ),
+                DownloadEntity(
+                    DOWNLOAD_ENTITY_3_ID,
+                    DOWNLOAD_ENTITY_3_VIDEO_KEY,
+                    DOWNLOAD_ENTITY_3_QUALITY,
+                    DOWNLOAD_ENTITY_3_TITLE,
+                    DOWNLOAD_ENTITY_3_AUTHOR,
+                    DOWNLOAD_ENTITY_3_THUMBNAIL_URL,
+                    DOWNLOAD_ENTITY_3_REQUESTED_TIMESTAMP,
+                    DOWNLOAD_ENTITY_3_DOWNLOAD_PATH,
+                    DOWNLOAD_ENTITY_3_STATUS,
+                    DOWNLOAD_ENTITY_3_SECONDS_LONG
+                ),
+                DownloadEntity(
+                    DOWNLOAD_ENTITY_4_ID,
+                    DOWNLOAD_ENTITY_4_VIDEO_KEY,
+                    DOWNLOAD_ENTITY_4_QUALITY,
+                    DOWNLOAD_ENTITY_4_TITLE,
+                    DOWNLOAD_ENTITY_4_AUTHOR,
+                    DOWNLOAD_ENTITY_4_THUMBNAIL_URL,
+                    DOWNLOAD_ENTITY_4_REQUESTED_TIMESTAMP,
+                    DOWNLOAD_ENTITY_4_DOWNLOAD_PATH,
+                    DOWNLOAD_ENTITY_4_STATUS,
+                    DOWNLOAD_ENTITY_4_SECONDS_LONG
+                )
             )
         }
     }
