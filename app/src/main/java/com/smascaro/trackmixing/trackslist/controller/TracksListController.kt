266de1b1db6c7a1460c2d1ed5683d47f4b6a747d
Sergi <msk1416@gmail.com>
2020-09-09 13:07:01 +0200

    Clean more unused code

--- app/src/main/java/com/smascaro/trackmixing/trackslist/controller/TracksListController.kt
+++ app/src/main/java/com/smascaro/trackmixing/trackslist/controller/TracksListController.kt
@@ -5,14 +5,10 @@ import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.smascaro.trackmixing.common.controller.BaseNavigatorController
 import com.smascaro.trackmixing.common.data.model.Track
 import com.smascaro.trackmixing.common.utils.NavigationHelper
-import com.smascaro.trackmixing.player.business.DownloadTrackUseCase
-import com.smascaro.trackmixing.trackslist.business.FetchAvailableTracksUseCase
 import com.smascaro.trackmixing.trackslist.business.FetchDownloadedTracks
 import com.smascaro.trackmixing.trackslist.view.TracksListViewMvc
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import timber.log.Timber
-import java.io.File
 import javax.inject.Inject
 
 
@@ -21,8 +17,7 @@ class TracksListController @Inject constructor(
     navigationHelper: NavigationHelper
 ) : BaseNavigatorController<TracksListViewMvc>(navigationHelper),
     TracksListViewMvc.Listener,
-    FetchAvailableTracksUseCase.Listener,
-    DownloadTrackUseCase.Listener, FetchDownloadedTracks.Listener {
+    FetchDownloadedTracks.Listener {
 
     override fun onSearchNavigationButtonClicked() {
         navigateToSearch()
@@ -53,42 +48,12 @@ class TracksListController @Inject constructor(
         navigateToDetails(track)
     }
 
-    override fun onAvailableTracksFetched(tracks: List<Track>) {
-        viewMvc.bindTracks(tracks)
-    }
-
-    override fun onAvailableTracksFetchFailed() {
-        Timber.e("Available tracks fetch failed")
-    }
-
-    override fun onDownloadTrackStarted(track: Track) {
-        Timber.i("Download of track ${track.videoKey} STARTED")
-    }
-
-    override fun onDownloadTrackFinished(track: Track, path: String) {
-        Timber.i("Download of track ${track.videoKey} FINISHED")
-        val downloadDirectory = File(path)
-        Timber.d("List of files in path $path:")
-        downloadDirectory.listFiles()?.forEach {
-            if (it.isDirectory) {
-                Timber.d("Dir: ${it.absolutePath}")
-            } else if (it.isFile) {
-                Timber.d("File: ${it?.absoluteFile}, size: ${it.length() / 1000}KB (${it.length() / 1000000}MB)")
-            }
-        }
-        navigateToPlayer(track)
-    }
-
     fun navigateToPlayer(track: Track) {
         navigationHelper.toPlayer(track)
     }
 
     fun navigateToDetails(track: Track) {
         navigationHelper.toDetails(track, FragmentNavigatorExtras())
-    }
-
-    override fun onDownloadTrackError() {
-        Timber.i("Download of track FAILED")
     }
 
     override fun onTracksFetched(tracks: List<Track>) {
