1cc6345c255702d367b8329fb1caa97d32f54b3e
Sergi <msk1416@gmail.com>
2020-09-27 13:23:36 +0200

    Migrate from MediaPlayer to ExoPlayer

---
+++ app/src/main/java/com/smascaro/trackmixing/playbackservice/utils/PlayerActions.kt
@@ -0,0 +1,15 @@
+package com.smascaro.trackmixing.playbackservice.utils
+
+import com.smascaro.trackmixing.playbackservice.model.TrackInstrument
+
+interface PlayerActions {
+    fun play()
+    fun play(instrument: TrackInstrument)
+    fun pause()
+    fun pause(instrument: TrackInstrument)
+    fun seek(newPosition: Long)
+    fun getVolume(instrument: TrackInstrument): Float
+    fun setVolume(volume: Int, instrument: TrackInstrument? = null)
+    fun getCurrentPosition(): Long
+    fun isCompleted(instrument: TrackInstrument): Boolean
+}
--- app/src/main/java/com/smascaro/trackmixing/playbackservice/utils/PlayingTrackState.kt
+++ app/src/main/java/com/smascaro/trackmixing/playbackservice/utils/PlayingTrackState.kt
@@ -1,18 +1,19 @@
 package com.smascaro.trackmixing.playbackservice.utils
 
-import android.media.MediaPlayer
+import android.content.Context
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.smascaro.trackmixing.common.data.model.Track
 import com.smascaro.trackmixing.common.utils.MEDIA_PLAYER_MAX_VOLUME
 import com.smascaro.trackmixing.common.view.architecture.BaseObservable
 import com.smascaro.trackmixing.playbackservice.model.TrackInstrument
-import java.io.File
-import java.io.FileInputStream
 import kotlin.math.ln
 
 class PlayingTrackState(
-    val instrument: TrackInstrument
-) : BaseObservable<PlayingTrackState.Listener>(), MediaPlayer.OnPreparedListener,
-    MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
+    val instrument: TrackInstrument,
+    private val context: Context
+) : BaseObservable<PlayingTrackState.Listener>(), Player.EventListener {
     interface Listener {
         fun onPlayerPrepared(instrument: TrackInstrument)
         fun onPlayerCompletion(instrument: TrackInstrument)
@@ -24,7 +25,7 @@ class PlayingTrackState(
         private val OTHER_FILENAME = "other.mp3"
         private val BASS_FILENAME = "bass.mp3"
         private val DRUMS_FILENAME = "drums.mp3"
-        fun create(track: Track, instrument: TrackInstrument): PlayingTrackState {
+        fun create(track: Track, instrument: TrackInstrument, context: Context): PlayingTrackState {
             val filename = when (instrument) {
                 TrackInstrument.VOCALS -> VOCALS_FILENAME
                 TrackInstrument.OTHER -> OTHER_FILENAME
@@ -32,7 +33,8 @@ class PlayingTrackState(
                 TrackInstrument.DRUMS -> DRUMS_FILENAME
             }
             val playingTrackState = PlayingTrackState(
-                instrument
+                instrument,
+                context
             ).apply {
                 initialize("${track.downloadPath}/$filename")
             }
@@ -45,14 +47,13 @@ class PlayingTrackState(
     }
 
     val maxVolume: Float = MEDIA_PLAYER_MAX_VOLUME
-    private var mCurrentMediaState: LogicMediaState =
-        LogicMediaState.PLAYING
     private var mIsPrepared: Boolean = false
-    private lateinit var mPlayer: MediaPlayer
+    private lateinit var player: ExoPlayer
     private var mVolume = maxVolume
     private var hasTrackCompletedPlaying: Boolean = false
+    private lateinit var progressiveMediaSourceFactory: ProgressiveMediaSource.Factory
     fun isPlaying(): Boolean {
-        return mCurrentMediaState == LogicMediaState.PLAYING
+        return player.isPlaying
     }
 
     var readyToPlay: Boolean = mIsPrepared
@@ -60,26 +61,51 @@ class PlayingTrackState(
         private set
 
     fun initialize(path: String) {
-        val fileStream = FileInputStream(File(path))
-        mPlayer = MediaPlayer()
-        mPlayer.setOnPreparedListener(this)
-        mPlayer.setOnCompletionListener(this)
-        mPlayer.setOnErrorListener(this)
-        mPlayer.setDataSource(fileStream.fd)
-        mPlayer.prepareAsync()
         hasTrackCompletedPlaying = false
+        val renderersFactory = DefaultRenderersFactory(context)
+        player = SimpleExoPlayer.Builder(context, renderersFactory).build()
+        player.playWhenReady = true
+        progressiveMediaSourceFactory =
+            ProgressiveMediaSource.Factory(DefaultDataSourceFactory(context))
+        val mediaSource = progressiveMediaSourceFactory.createMediaSource(MediaItem.fromUri(path))
+        player.setMediaSource(mediaSource)
+        player.prepare()
+        player.addListener(this)
     }
 
-    fun incrementVolume() {
-        mVolume += 0.01f
-        mVolume.coerceAtMost(1.0f)
-        updateVolume()
+
+    override fun onPlaybackStateChanged(state: Int) {
+        super.onPlaybackStateChanged(state)
+        when (state) {
+            SimpleExoPlayer.STATE_BUFFERING,
+            SimpleExoPlayer.STATE_READY,
+            SimpleExoPlayer.STATE_IDLE -> {
+            }
+            SimpleExoPlayer.STATE_ENDED -> handleTrackCompletion()
+        }
     }
 
-    fun decrementVolume() {
-        mVolume -= 0.01f
-        mVolume.coerceAtLeast(0.0f)
-        updateVolume()
+    private fun handleTrackCompletion() {
+        hasTrackCompletedPlaying = true
+        getListeners().forEach {
+            it.onPlayerCompletion(instrument)
+        }
+    }
+
+    override fun onIsLoadingChanged(isLoading: Boolean) {
+        if (!isLoading) {
+            mIsPrepared = true
+            getListeners().forEach {
+                it.onPlayerPrepared(instrument)
+            }
+        } else {
+            mIsPrepared = false
+        }
+    }
+
+    override fun onPlayerError(error: ExoPlaybackException) {
+        super.onPlayerError(error)
+        getListeners().forEach { it.onPlayerError(instrument, error.localizedMessage) }
     }
 
     fun setVolume(volume: Int) {
@@ -87,38 +113,23 @@ class PlayingTrackState(
         updateVolume()
     }
 
-    fun mute() {
-        mVolume = 0.0f
-        updateVolume()
-        mCurrentMediaState =
-            LogicMediaState.MUTED
-    }
-
-    fun unmute() {
-        mVolume = 1.0f
-        updateVolume()
-        mCurrentMediaState =
-            LogicMediaState.PLAYING
-    }
-
-    /**
-     * To be called after initialize or an error will happen
-     */
     fun play() {
-        if (mIsPrepared && !mPlayer.isPlaying) {
-            mPlayer.start()
+        player.playbackLooper.thread.run {
+            player.play()
         }
     }
 
     fun pause() {
-        if (mPlayer.isPlaying) {
-            mPlayer.pause()
+        player.playbackLooper.thread.run {
+            player.pause()
         }
     }
 
     fun finalize() {
-        mPlayer.release()
         mIsPrepared = false
+        player.stop()
+        player.release()
+        player.removeListener(this)
     }
 
     fun isCompleted(): Boolean {
@@ -127,59 +138,18 @@ class PlayingTrackState(
 
     private fun updateVolume() {
         val mediaPlayerVolume = (1 - (ln(maxVolume - mVolume) / ln(maxVolume)))
-        mPlayer.setVolume(mediaPlayerVolume, mediaPlayerVolume)
-        if (mVolume == 0.0f) {
-            mCurrentMediaState =
-                LogicMediaState.MUTED
-        } else if (mVolume > 0.0f && mCurrentMediaState == LogicMediaState.MUTED) {
-            mCurrentMediaState =
-                LogicMediaState.PLAYING
-        }
-    }
-
-    override fun onPrepared(mp: MediaPlayer?) {
-        mIsPrepared = true
-        getListeners().forEach {
-            it.onPlayerPrepared(instrument)
-        }
-    }
-
-    override fun onCompletion(mp: MediaPlayer?) {
-        hasTrackCompletedPlaying = true
-        getListeners().forEach {
-            it.onPlayerCompletion(instrument)
-        }
-    }
-
-    override fun onError(mp: MediaPlayer?, what: Int, extra: Int): Boolean {
-        mIsPrepared = false
-        val whatMsg = when (what) {
-            MediaPlayer.MEDIA_ERROR_UNKNOWN -> "Unknown error"
-            MediaPlayer.MEDIA_ERROR_SERVER_DIED -> "Server died"
-            else -> "Unrecognized error code"
-        }
-        val extraMsg = when (extra) {
-            MediaPlayer.MEDIA_ERROR_IO -> "I/O Error"
-            MediaPlayer.MEDIA_ERROR_MALFORMED -> "Malformed data"
-            MediaPlayer.MEDIA_ERROR_UNSUPPORTED -> "Unsupported format"
-            MediaPlayer.MEDIA_ERROR_TIMED_OUT -> "Timed out"
-            else -> "Unrecognized extra error code"
-        }
-        getListeners().forEach {
-            it.onPlayerError(instrument, "$whatMsg - $extraMsg")
-        }
-        return true
+        player.audioComponent?.volume = mediaPlayerVolume
     }
 
     fun getVolume(): Float {
         return mVolume
     }
 
-    fun getTimestampMillis(): Int {
-        return mPlayer.currentPosition
+    fun getTimestampMillis(): Long {
+        return player.currentPosition
     }
 
-    fun seek(timestampSeconds: Int) {
-        mPlayer.seekTo(timestampSeconds * 1000)
+    fun seek(timestampSeconds: Long) {
+        player.seekTo(timestampSeconds * 1000)
     }
 }
