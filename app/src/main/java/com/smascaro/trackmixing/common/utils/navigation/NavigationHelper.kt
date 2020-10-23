7c579de001e77a604681f896bb6f1290ad675675
Sergi <msk1416@gmail.com>
2020-10-23 10:06:46 +0200

    Remove Navigation listener, no longer used

--- app/src/main/java/com/smascaro/trackmixing/common/utils/navigation/NavigationHelper.kt
+++ app/src/main/java/com/smascaro/trackmixing/common/utils/navigation/NavigationHelper.kt
@@ -2,14 +2,9 @@ package com.smascaro.trackmixing.common.utils.navigation
 
 import androidx.navigation.NavController
 import com.smascaro.trackmixing.common.data.model.Track
-import com.smascaro.trackmixing.common.view.architecture.Observable
 import com.smascaro.trackmixing.settings.business.downloadtestdata.selection.model.TestDataBundleInfo
 
-interface NavigationHelper : Observable<NavigationHelper.Listener> {
-    interface Listener {
-        fun onDestinationChange(destination: NavigationDestination)
-    }
-
+interface NavigationHelper {
     fun navigateTo(destination: Int)
     fun bindNavController(navController: NavController)
     fun toPlayer(track: Track)
