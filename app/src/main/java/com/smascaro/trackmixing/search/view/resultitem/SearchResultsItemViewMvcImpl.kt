4d90306521522e3cdd648e18044a79bd1d4d8dfb
Sergi <msk1416@gmail.com>
2020-11-01 19:35:18 +0100

    Refactor list item to a more performant layout by  - Use RelativeLayout instead of ConstraintLayout  - Combine 5 views into one single view

--- app/src/main/java/com/smascaro/trackmixing/search/view/resultitem/SearchResultsItemViewMvcImpl.kt
+++ app/src/main/java/com/smascaro/trackmixing/search/view/resultitem/SearchResultsItemViewMvcImpl.kt
@@ -5,28 +5,28 @@ import com.bumptech.glide.RequestManager
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.textview.MaterialTextView
 import com.smascaro.trackmixing.R
+import com.smascaro.trackmixing.common.utils.ResourcesWrapper
 import com.smascaro.trackmixing.common.utils.TimeHelper
 import com.smascaro.trackmixing.common.view.architecture.BaseObservableViewMvc
 import com.smascaro.trackmixing.search.model.SearchResult
 
-class SearchResultsItemViewMvcImpl(private val glide: RequestManager) :
+class SearchResultsItemViewMvcImpl(
+    private val glide: RequestManager,
+    private val resources: ResourcesWrapper
+) :
     BaseObservableViewMvc<SearchResultsItemViewMvc.Listener>(),
     SearchResultsItemViewMvc {
     private lateinit var searchResult: SearchResult
     private var position: Int = -1
     private lateinit var searchResultThumbnailImageView: ImageView
     private lateinit var searchResultTitleTextView: MaterialTextView
-    private lateinit var searchResultAuthorTextView: MaterialTextView
-    private lateinit var searchResultDurationTextView: MaterialTextView
-    private lateinit var searchResultStatusTextView: MaterialTextView
+    private lateinit var searchResultDetailsTextView: MaterialTextView
 
     override fun initialize() {
         super.initialize()
         searchResultThumbnailImageView = findViewById(R.id.iv_item_track_thumbnail)
         searchResultTitleTextView = findViewById(R.id.tv_item_track_title)
-        searchResultAuthorTextView = findViewById(R.id.tv_item_track_author)
-        searchResultDurationTextView = findViewById(R.id.tv_item_track_duration)
-        searchResultStatusTextView = findViewById(R.id.tv_item_track_status)
+        searchResultDetailsTextView = findViewById(R.id.tv_item_track_details)
     }
 
     override fun initializeListeners() {
@@ -46,9 +46,12 @@ class SearchResultsItemViewMvcImpl(private val glide: RequestManager) :
     override fun bindResult(result: SearchResult) {
         this.searchResult = result
         searchResultTitleTextView.text = this.searchResult.title
-        searchResultAuthorTextView.text = this.searchResult.author
-        searchResultDurationTextView.text =
+        val authorText = this.searchResult.author
+        val durationText =
             TimeHelper.fromSeconds(this.searchResult.secondsLong.toLong()).toStringRepresentation()
+        val statusText = "Tap to download"
+        searchResultDetailsTextView.text =
+            resources.getString(R.string.track_item_data_template, authorText, durationText, statusText)
         glide
             .asBitmap()
             .load(this.searchResult.thumbnailUrl)
