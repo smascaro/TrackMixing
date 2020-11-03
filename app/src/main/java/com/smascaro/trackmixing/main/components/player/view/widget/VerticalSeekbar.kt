04896e9ec704d09448d599b3fb06749dbe7a5ac5
Sergi <msk1416@gmail.com>
2020-11-03 15:52:10 +0100

    Update aspect of VerticalSeekbar to show numerical progress

--- app/src/main/java/com/smascaro/trackmixing/main/components/player/view/widget/VerticalSeekbar.kt
+++ app/src/main/java/com/smascaro/trackmixing/main/components/player/view/widget/VerticalSeekbar.kt
@@ -2,32 +2,81 @@ package com.smascaro.trackmixing.main.components.player.view.widget
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatSeekBar
+import com.smascaro.trackmixing.R
 
-class VerticalSeekbar : AppCompatSeekBar {
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
-    constructor(context: Context, attributeSet: AttributeSet, defstyle: Int) : super(
-        context,
-        attributeSet,
-        defstyle
-    )
+class VerticalSeekbar(context: Context, attributeSet: AttributeSet) :
+    AppCompatSeekBar(context, attributeSet) {
+
+    var showNumericProgress: Boolean = false
+
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.WHITE
+        textSize = 18f * context.resources.displayMetrics.scaledDensity
+        style = Paint.Style.FILL
+    }
+
+    private var text = ""
+    private val textRect = Rect()
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attributeSet,
+            R.styleable.VerticalSeekbar,
+            0, 0
+        ).apply {
+            try {
+                showNumericProgress = getBoolean(R.styleable.VerticalSeekbar_showNumericProgress, false)
+            } finally {
+                recycle()
+            }
+        }
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(h, w, oldh, oldw)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        setMeasuredDimension(measuredHeight, measuredWidth)
+        val width = suggestedMinimumWidth + paddingLeft + paddingRight
+        val height = suggestedMinimumHeight + paddingTop + paddingBottom
+        setMeasuredDimension(
+            getDefaultSize(width, widthMeasureSpec),
+            getDefaultSize(height, heightMeasureSpec)
+        )
     }
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.rotate(-90f)
         canvas?.translate(-height.toFloat(), 0f)
         super.onDraw(canvas)
+
+        val bottomBorderY = paddingStart
+        canvas?.save()
+        if (showNumericProgress) {
+            text = "$progress"
+            val percentageProgress = (progress.toFloat() / max.toFloat()) //0 to 1
+            val textX = width * 0.5f
+            val textY = height.toFloat() * percentageProgress - 2 * paddingEnd * percentageProgress
+            textPaint.getTextBounds(text, 0, text.length, textRect)
+            textPaint.color = if (textY < bottomBorderY) {
+                Color.WHITE
+            } else {
+                Color.BLACK
+            }
+            canvas?.translate(
+                textY,
+                textX - textRect.exactCenterX()
+            )
+            canvas?.rotate(90f)
+            canvas?.drawText(text, 0f, 0f, textPaint)
+            canvas?.restore()
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
