9ff201d2422c0c64fd6dbe54e5b9cc2d783cff8e
Sergi <msk1416@gmail.com>
2020-11-07 02:01:59 +0100

    Create buildSrc module

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,13 @@
+object Artifacts {
+    const val timber = "com.jakewharton.timber:timber"
+}
+
+object Versions {
+    const val timber = "4.7.1"
+}
+
+object Dependencies{
+    val libs = mapOf(
+        Artifacts.timber to Versions.timber
+    )
+}
