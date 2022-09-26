ed61afa7f1f8b77598f55c44410cb3886df42638
Sergi Mascaró <sergi@chordify.net>
2022-09-26 23:37:15 +0200

    Rename UiUtils to KeyboardUtils

--- app/src/main/java/com/smascaro/trackmixing/base/utils/KeyboardUtils.kt
+++ app/src/main/java/com/smascaro/trackmixing/base/utils/KeyboardUtils.kt
@@ -7,20 +7,6 @@ import android.view.inputmethod.InputMethodManager
 import androidx.viewbinding.ViewBinding
 import javax.inject.Inject
 
-class UiUtils @Inject constructor(private val context: Context) {
-    fun hideKeyboard(rootView: View) {
-        val imm =
-            context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(rootView.windowToken, 0)
-    }
-
-    fun showKeyboard() {
-        val imm =
-            context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-    }
-}
-
 object KeyboardUtils {
 
     fun show(binding: ViewBinding) = show(binding.root)
